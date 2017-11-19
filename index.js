cc12ae2a1c0b56143f1c7581a8171f41e8056398
Rossiel González <alu0100763478@ull.edu.es>
2017-11-19 12:53:41 +0000

    Añadido al servidor las peticiones get,post,put y delete

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.get('/hola', (req,res) => {
-	res,send({message: 'HOLA MUNDI!'})
+app.get('/api/product', (req,res) => {
+	res.send(200,{ productos: []})
+})
+
+app.get('/api/product/:producID', (req,res) => {
+})
+
+app.post('/api/product', (req,res) => {
+	console.log(req.body)
+	res.send(200,{mensagge: 'El producto se ha recibido'})
+})
+
+app.put('/api/product/:producID', (req,res) => {
+})
+
+app.delete('/api/product/:producID', (req,res) => {
 })
 
 app.listen(port,() =>{
