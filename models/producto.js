480c09ed953bcc87bc466791339ea74f8615adf1
alu0100763478 <alu0100763478@ull.edu.es>
2018-01-15 18:36:48 +0000

    Tabla de libros terminada

--- models/producto.js
+++ models/producto.js
@@ -5,12 +5,14 @@ const Schema = mongoose.Schema
 
 const LibrosSchema = Schema({
   name: String,
+  autor: String,
   picture: String,
   categoria: { type: String, enum:['Policial','Romántica','Aventura','Terror'
   ,'Ficción','Investigación','Biográfica','Infantil','Autoayuda','Erótica'
   ,'Hogar','Enciclopedia','Política','Economía','Sociedad','Deportes','Viajes'
   ,'Otros temas']},
   descripcion: String,
+  year: Number,
   url: String
 })
 
